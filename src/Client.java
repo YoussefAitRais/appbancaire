01a4bccdc047a918f29087e46e4d5ca26a6f18bc
Youssef Ait Rais <copybuisness007@gmail.com>
2024-12-24 15:52:42 +0200

    constructur

--- src/Client.java
+++ src/Client.java
@@ -3,7 +3,15 @@ public class Client {
     private int id;
     private String nom, prenom, adresse, email, tel;
 
-
+public Client (String nom , String prenom , String adresse , String email , String tel)
+{
+    this.nom = nom;
+    this.prenom = prenom;
+    this.adresse = adresse;
+    this.email = email;
+    this.tel = tel;
+    
+}
 
 
 
